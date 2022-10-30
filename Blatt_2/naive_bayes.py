d51dc4d8a06d201015c88392d62574b708ce9246
Florian Natterer <natterer.flo99@gmail.com>
2022-10-30 15:34:18 +0100

    setup of naive_bayes implementation

--- Blatt_2/naive_bayes.py
+++ Blatt_2/naive_bayes.py
@@ -3,32 +3,30 @@ import numpy as np
 from typing import Optional
 
 
-def _get_a_priori_probabilities(Y: np.ndarray):
-    classes, counts = np.unique(Y, return_counts=True)
-    relative_frequencies = counts / len(Y)
-    return {"class": classes, "priori": relative_frequencies}
-
-
 class GaussianNaiveBayes():
     """
     Naive Bayes Classifier under the assumption of gaussian distributed classes
     """
 
-    def __int__(self, priors: Optional[np.ndarray] = None):
+    def __init__(self, priors: Optional[np.ndarray] = None):
         self.fitted: bool = False
         self.priors = None
-
+        self._co
     def fit(self, X: np.ndarray, Y: np.ndarray):
         """
         Fit training data to the Naive Bayes Classifier
         """
         if self.priors is None:
+            self.priors = self._get_a_priori_probabilities(Y)
 
-    # TODO: Implement Method to estimate a-priori Probability of the Classes
     pass
 
     def predict(self, X: np.ndarray, probability: bool = False) -> np.ndarray:
         """
+
+        :param X: Data
+        :param probability: If True predict returns class membership probabilities if False returns just the labels
+        :return: Predictions
         """
         if not self.fitted:
             raise ValueError("Estimator has not yet been fitted!")
@@ -37,8 +35,6 @@ class GaussianNaiveBayes():
         else:
             return self._predict_scores(X)
 
-    pass
-
     def _predict_proba(self, X: np.ndarray) -> np.ndarray:
         # TODO Implement
         pass
@@ -46,6 +42,12 @@ class GaussianNaiveBayes():
     def _predict_scores(self, X: np.ndarray) -> np.ndarray:
         # TODO Implement
         pass
+
+    @staticmethod
+    def _get_a_priori_probabilities(Y: np.ndarray) -> dict[str, float]:
+        classes, counts = np.unique(Y, return_counts=True)
+        relative_frequencies = counts / len(Y)
+        return {"class": classes, "priori": relative_frequencies}
 
     def _get_class_seperations(self, X: np.ndarray):
         # TODO Implement
